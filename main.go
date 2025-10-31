9b4c1c7f013160d65b336700f33042888bf9cfaf
Veerendra <8393701+veerendra2@users.noreply.github.com>
2025-10-31 00:03:29 +0000

    Update Template

--- main.go
+++ main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"log/slog"
+	"os"
 
 	"github.com/alecthomas/kong"
 	"github.com/veerendra2/gopackages/slogger"
+	"github.com/veerendra2/gopackages/version"
 )
 
 var cli struct {
 	Log slogger.Config `embed:"" prefix:"log." envprefix:"LOG_"`
+
+	Version bool `help:"Show app version and exit"`
 }
 
 func main() {
-	kongCtx := kong.Parse(&cli)
+	kongCtx := kong.Parse(&cli,
+		kong.Description("My app."),
+	)
+
+	if cli.Version {
+		println(version.Version)
+		os.Exit(0)
+	}
+
 	kongCtx.FatalIfErrorf(kongCtx.Error)
 
 	slog.SetDefault(slogger.New(cli.Log))
-	slog.Info("Info log")
-	slog.Warn("Warning log")
-	slog.Debug("Debug log")
-	slog.Error("Error log")
+
+	slog.Info("Version information", version.Info())
+	slog.Info("Build context", version.BuildContext())
 }
